b1f7f8cff52ad8f7d7ffa7801c1ad8d0b1b5cbb7
Manoj-Mahat0 <codeinlastbench@gmail.com>
2025-09-11 22:06:56 +0530

    Local

--- oddo-frontend/src/api/client.js
+++ oddo-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",//local server
+  baseURL: "http://ofcbackend.globalinfosoft.co:8003",//local server
   // baseURL: "https://oddo-bhe0.onrender.com",//online server
 });
 
